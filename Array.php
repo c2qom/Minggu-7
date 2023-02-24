e75a18b90ec4436914aa2c2a7df36ae1046c48e8
c2qom <c2kkqom@gmail.com>
2023-02-24 13:31:25 +0700

    Add files via upload
    Belajar array 1

---
+++ Array.php
@@ -0,0 +1,18 @@
+<?php
+
+//Cara lama
+$hari = array("Senin", "Selasa", "Rabu");
+//Memanggil array
+// Var_dump($hari);
+// Echo $hari[0];
+// print_r($hari);  
+// echo "<br>";
+
+// //Cara baru
+// $bulan = ["Januari", "Februari", "Maret"];
+// print_r($bulan); 
+
+print_r($hari);
+$hari[] = "Kamis";
+echo "<br>";
+print_r($hari);
