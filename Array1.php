b89bff7fef68701fa89b32e606da0b732cc0403a
c2qom <c2kkqom@gmail.com>
2023-02-24 13:32:11 +0700

    Add files via upload
    Belajar array 2

---
+++ Array1.php
@@ -0,0 +1,47 @@
+<?php
+// perulangan pada array
+// for atau foreach
+$angka = [1, 3, 5, 7, 9, 11, 13, 17];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="wiewpart"
+    content="width=device-tidth, initial-scale=1.0">
+    <title>Latihan Array</title>
+    <style>
+        .kotak{
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+            margin: 3px;
+            float: left;
+        }
+        .clear{
+            clear: both
+        }
+    </style>
+</head>
+
+<body>
+        <?php for ($i = 0; $i < 7; $i++) {?>
+        <div class="kotak">
+            <?php echo $angka[$i]; ?>
+        </div>
+        <?php } ?>
+        <div class="clear"></div>
+        <?php foreach ($angka as $a) : ?>
+            <div class="kotak">
+                <?= $a; ?>
+        </div>
+        <?php endforeach; ?>
+</body>
+</html>
+
+
+
